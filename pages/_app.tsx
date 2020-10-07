920c0c9b99350f4412895d451adbda8b60c213e1
Rauventa <rauventa@gmail.com>
2020-10-07 07:27:53 +0300

    testing pages, TS errors here

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+// import { Provider } from 'react-redux'
+//
+// export default function App({ Component, pageProps }) {
+//     const store = useStore(pageProps.initialReduxState)
+//
+//     return (
+//         <Provider store={store}>
+//             <Component {...pageProps} />
+//         </Provider>
+//     )
+// }
